534b717ccf9d5e1e32e96548894c0e17897603fc
nick momotenko <nickmomotenkoforweb@gmail.com>
2019-12-25 22:37:43 +0200

    add drag and drop for test

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Popup from '../Popup';
 
 import './Sidebar.scss';
@@ -17,6 +17,61 @@ const Sidebar = ({
     activeItem,
     setActiveItem
 }) => {
+
+    useEffect(() => {
+        sortable();
+    })
+
+    let sortable = () => {
+        let dragElement;
+
+        let list = document.querySelector('.sidebar__list');
+        let listChilden = list.children;
+        let listArray  = Array.from(listChilden);
+
+        listArray.map(item => {
+            item.draggable = true;
+        });
+
+        
+        
+        let onDragOver = event => {
+            event.preventDefault();
+            event.dataTransfer.dropEffect = "move";
+
+            let target = event.target;
+
+            if (target && target !== dragElement && target.nodeName == 'LI') {
+                list.insertBefore(dragElement,  target.nextSibling);
+            }
+        }
+
+        let onDragEnd = event => {
+            event.preventDefault();
+
+            dragElement.classList.remove('ghost');
+            list.removeEventListener('dragover', onDragOver, false);
+            list.removeEventListener('dragend', onDragEnd, false);
+        }
+
+        list.addEventListener('dragstart', event => {
+            dragElement = event.target; // Запоминаем элемент который будет перемещать
+
+            // Ограничиваем тип перетаскивания
+            event.dataTransfer.effectAllowed = 'move';
+            event.dataTransfer.setData('Text', dragElement.textContent);
+
+            // Пописываемся на события при dnd
+            list.addEventListener('dragover', onDragOver, false);
+            list.addEventListener('dragend', onDragEnd, false);
+
+            setTimeout(function () {
+                // Если выполнить данное действие без setTimeout, то
+                // перетаскиваемый объект, будет иметь этот класс.
+                dragElement.classList.add('ghost');
+            }, 0)
+        })
+    }
 
     return (
         <div className="sidebar">
@@ -52,16 +107,16 @@ const Sidebar = ({
                     Добавить папку
                 </button>
                 {
-                    isActivePopup 
-                        && 
-                            <Popup 
-                                setIsActivePopup={setIsActivePopup} 
-                                inputValueFromPopup={inputValueFromPopup}
-                                setInputValueFromPopup={setInputValueFromPopup}
-                                addNewList={addNewList}
-                                activeColor={activeColor}
-                                setActiveColor={setActiveColor}
-                            />
+                    isActivePopup
+                    &&
+                    <Popup
+                        setIsActivePopup={setIsActivePopup}
+                        inputValueFromPopup={inputValueFromPopup}
+                        setInputValueFromPopup={setInputValueFromPopup}
+                        addNewList={addNewList}
+                        activeColor={activeColor}
+                        setActiveColor={setActiveColor}
+                    />
                 }
             </div>
         </div>
@@ -73,23 +128,21 @@ export default Sidebar;
 
 const SidebarItem = ({ item, activeItem, deleteItem, setActiveItem }) => {
     return (
-        <li className={activeItem === item ? 'sidebar__item sidebar__item--active' : 'sidebar__item'} key>
-            <a href="#" className="sidebar__link" onClick={e => { setActiveItem(item) }}>
-                <span className="sidebar__color-bar" style={{ background: `${item.color} ` }}></span>
-                <div className="sidebar__title">
-                    {item.name}
-                </div>
-                <button
-                    className="button"
-                    onClick={e => {
-                        e.preventDefault();
-                        deleteItem(item);
-                    }}
-                    style={{opacity : activeItem === item ? 1 : 0}}
-                >
-                    <img src={button_cross} alt="" className="button__cross"/>
-                </button>
-            </a>
+        <li className={activeItem === item ? 'sidebar__item sidebar__item--active' : 'sidebar__item'} key onClick={e => { setActiveItem(item) }}>
+            <span className="sidebar__color-bar" style={{ background: `${item.color} ` }}></span>
+            <div className="sidebar__title">
+                {item.name}
+            </div>
+            <button
+                className="button"
+                onClick={e => {
+                    e.preventDefault();
+                    deleteItem(item);
+                }}
+                style={{ opacity: activeItem === item ? 1 : 0 }}
+            >
+                <img src={button_cross} alt="" className="button__cross" />
+            </button>
         </li>
     )
 }
